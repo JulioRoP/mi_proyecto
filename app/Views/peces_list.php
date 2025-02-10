b1abc179570ecb99b763df4d889a0f348a7ef67a
Julio <julioromeroparada@alumnos.alborfp.com>
2025-02-10 14:12:06 +0100

    delete filter

--- app/Views/peces_list.php
+++ app/Views/peces_list.php
@@ -862,18 +862,8 @@ License: For each use you must have a valid license purchased only from above li
 									<div class="card-header border-0 pt-6">
 										<!--begin::Card title-->
 										<div class="card-title">
-											<!--begin::Search-->
-											<div class="d-flex align-items-center position-relative my-1">
-												<!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
-												<span class="svg-icon svg-icon-1 position-absolute ms-6">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-														<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
-														<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
-													</svg>
-												</span>
-												<!--end::Svg Icon-->
-												<input type="text" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Search user" />
-											</div>
+											<!--begin::buscador quitado-->
+											
 											<!--end::Search-->
 										</div>
 										<!--begin::Card title-->
@@ -904,7 +894,19 @@ License: For each use you must have a valid license purchased only from above li
 													<div class="px-7 py-5" data-kt-user-table-filter="form">
 														<!--begin::Input group-->
 														<div class="mb-10">
-															<label class="form-label fs-6 fw-bold">Role:</label>
+															
+															
+
+
+
+
+
+
+
+
+
+
+															<!-- <label class="form-label fs-6 fw-bold">Role:</label>
 															<select class="form-select form-select-solid fw-bolder" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-user-table-filter="role" data-hide-search="true">
 																<option></option>
 																<option value="Administrator">Administrator</option>
@@ -912,28 +914,40 @@ License: For each use you must have a valid license purchased only from above li
 																<option value="Developer">Developer</option>
 																<option value="Support">Support</option>
 																<option value="Trial">Trial</option>
-															</select>
+															</select> -->
 														</div>
 														<!--end::Input group-->
 														<!--begin::Input group-->
-														<div class="mb-10">
+														<!-- <div class="mb-10">
 															<label class="form-label fs-6 fw-bold">Two Step Verification:</label>
 															<select class="form-select form-select-solid fw-bolder" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-user-table-filter="two-step" data-hide-search="true">
 																<option></option>
 																<option value="Enabled">Enabled</option>
 															</select>
-														</div>
+														</div> -->
 														<!--end::Input group-->
 														<!--begin::Actions-->
-														<div class="d-flex justify-content-end">
+														<!-- <div class="d-flex justify-content-end">
 															<button type="reset" class="btn btn-light btn-active-light-primary fw-bold me-2 px-6" data-kt-menu-dismiss="true" data-kt-user-table-filter="reset">Reset</button>
 															<button type="submit" class="btn btn-primary fw-bold px-6" data-kt-menu-dismiss="true" data-kt-user-table-filter="filter">Apply</button>
-														</div>
-														<!--end::Actions-->
+														</div> -->
+														<!-- end::Actions -->
 													</div>
 													<!--end::Content-->
 												</div>
 												<!--end::Menu 1-->
+
+
+
+
+
+
+
+
+
+
+
+
 												<!--end::Filter-->
 												<!--begin::Export-->
 												<button type="button" class="btn btn-light-primary me-3" data-bs-toggle="modal" data-bs-target="#kt_modal_export_users">
