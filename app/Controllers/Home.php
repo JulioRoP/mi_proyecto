44d1ec0dbf09e1a0cf6824aafca4c1e1d55a7ba0
Julio <julioromeroparada@alumnos.alborfp.com>
2025-02-10 13:51:24 +0100

    Primer commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,8 +2,21 @@
 
 namespace App\Controllers;
 
+/**Class Home
+ * 
+ * [Controlador principal ,este maneja la vista inicial de la aplicación.
+ * 
+ * @ App\Controllers
+ */
 class Home extends BaseController
 {
+    /**Método index
+     * 
+     * [El metodo index devolvera la vista principal de la aplicación.]
+     *
+     * @return string Devuelve vista de la página principal para poder ser monstrada.
+     * 
+     */
     public function index(): string
     {
         return view('index');
