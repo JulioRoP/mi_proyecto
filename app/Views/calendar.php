e425bb2c345b20436e805afb2f92ba598a2ea716
Julio <julioromeroparada@alumnos.alborfp.com>
2025-02-25 12:53:23 +0100

    calendario limpio

--- app/Views/calendar.php
+++ app/Views/calendar.php
@@ -53,6 +53,7 @@ License: For each use you must have a valid license purchased only from above li
 	<link href="../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
 	<!--end::Global Stylesheets Bundle-->
 	<link href="path/to/metronic/assets/css/style.bundle.css" rel="stylesheet">
+
 	<!-- FullCalendar CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.css" rel="stylesheet">
 </head>
@@ -321,31 +322,244 @@ License: For each use you must have a valid license purchased only from above li
 									<!--end::Wrapper-->
 									<!--begin::Button-->
 									<!--end::Button-->
-									
+
 								</div>
-	
+
 							</div>
 							<!--end::Container-->
 						</div>
 						<!--end::Toolbar-->
 						<!--begin::Post-->
-						<div class="g-5 gx-xxl-8">
-							<div class="card card-xxl-stretch">
-								<div class="card-body">
-								<div class="container mt-5">
-        <h2>Calendario Dinámico</h2>
-        <div id="calendar"></div>
-    </div>
+
+
+
+
+
+
+
+
+
+						<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+							<div class="post d-flex flex-column-fluid" id="kt_post">
+								<div id="kt_content_container" class="container-xxl">
+									<!--begin::Row-->
+									<div class="row gy-5 g-xl-8">
+										<!--begin::Col-->
+
+										<!--begin::Mixed Widget 2-->
+										<div class="card card-xxl-stretch">
+											<div class="container mt-5">
+												<h2>Calendario Dinámico
+													<div class="d-flex justify-content-between">
+														<div></div>
+														<button class="btn btn-flex btn-primary" data-kt-calendar="add">
+															<!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+															<span class="svg-icon svg-icon-2">
+																<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+																	<rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1" transform="rotate(-90 11.364 20.364)" fill="black" />
+																	<rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black" />
+																</svg>
+															</span>
+															<!--end::Svg Icon-->Add Event
+														</button>
+													</div>
+												</h2>
+												<div id="calendar"></div>
+											</div><br>
+
+											<div class="modal fade" id="kt_modal_add_event" tabindex="-1" aria-hidden="true">
+												<!--begin::Modal dialog-->
+												<div class="modal-dialog modal-dialog-centered mw-650px">
+													<!--begin::Modal content-->
+													<div class="modal-content">
+														<!--begin::Form-->
+														<form class="form" action="#" id="kt_modal_add_event_form">
+															<!--begin::Modal header-->
+															<div class="modal-header">
+																<!--begin::Modal title-->
+																<h2 class="fw-bolder" data-kt-calendar="title">Add Event</h2>
+																<!--end::Modal title-->
+																<!--begin::Close-->
+																<div class="btn btn-icon btn-sm btn-active-icon-primary" id="kt_modal_add_event_close">
+																	<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+																	<span class="svg-icon svg-icon-1">
+																		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+																			<rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+																			<rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+																		</svg>
+																	</span>
+																	<!--end::Svg Icon-->
+																</div>
+																<!--end::Close-->
+															</div>
+															<!--end::Modal header-->
+															<!--begin::Modal body-->
+															<div class="modal-body py-10 px-lg-17">
+																<!--begin::Input group-->
+																<div class="fv-row mb-9">
+																	<!--begin::Label-->
+																	<label class="fs-6 fw-bold required mb-2">Event Name</label>
+																	<!--end::Label-->
+																	<!--begin::Input-->
+																	<input type="text" class="form-control form-control-solid" placeholder="" name="calendar_event_name" />
+																	<!--end::Input-->
+																</div>
+																<!--end::Input group-->
+																<!--begin::Input group-->
+																<div class="fv-row mb-9">
+																	<!--begin::Label-->
+																	<label class="fs-6 fw-bold mb-2">Event Description</label>
+																	<!--end::Label-->
+																	<!--begin::Input-->
+																	<input type="text" class="form-control form-control-solid" placeholder="" name="calendar_event_description" />
+																	<!--end::Input-->
+																</div>
+																<!--end::Input group-->
+																<!--begin::Input group-->
+																<div class="fv-row mb-9">
+																	<!--begin::Label-->
+																	<label class="fs-6 fw-bold mb-2">Event Location</label>
+																	<!--end::Label-->
+																	<!--begin::Input-->
+																	<input type="text" class="form-control form-control-solid" placeholder="" name="calendar_event_location" />
+																	<!--end::Input-->
+																</div>
+																<!--end::Input group-->
+																<!--begin::Input group-->
+																<div class="fv-row mb-9">
+																	<!--begin::Checkbox-->
+																	<label class="form-check form-check-custom form-check-solid">
+																		<input class="form-check-input" type="checkbox" value="" id="kt_calendar_datepicker_allday" />
+																		<span class="form-check-label fw-bold" for="kt_calendar_datepicker_allday">All Day</span>
+																	</label>
+																	<!--end::Checkbox-->
+																</div>
+																<!--end::Input group-->
+																<!--begin::Input group-->
+																<div class="row row-cols-lg-2 g-10">
+																	<div class="col">
+																		<div class="fv-row mb-9">
+																			<!--begin::Label-->
+																			<label class="fs-6 fw-bold mb-2 required">Event Start Date</label>
+																			<!--end::Label-->
+																			<!--begin::Input-->
+																			<input class="form-control form-control-solid" name="calendar_event_start_date" placeholder="Pick a start date" id="kt_calendar_datepicker_start_date" />
+																			<!--end::Input-->
+																		</div>
+																	</div>
+																	<div class="col" data-kt-calendar="datepicker">
+																		<div class="fv-row mb-9">
+																			<!--begin::Label-->
+																			<label class="fs-6 fw-bold mb-2">Event Start Time</label>
+																			<!--end::Label-->
+																			<!--begin::Input-->
+																			<input class="form-control form-control-solid" name="calendar_event_start_time" placeholder="Pick a start time" id="kt_calendar_datepicker_start_time" />
+																			<!--end::Input-->
+																		</div>
+																	</div>
+																</div>
+																<!--end::Input group-->
+																<!--begin::Input group-->
+																<div class="row row-cols-lg-2 g-10">
+																	<div class="col">
+																		<div class="fv-row mb-9">
+																			<!--begin::Label-->
+																			<label class="fs-6 fw-bold mb-2 required">Event End Date</label>
+																			<!--end::Label-->
+																			<!--begin::Input-->
+																			<input class="form-control form-control-solid" name="calendar_event_end_date" placeholder="Pick a end date" id="kt_calendar_datepicker_end_date" />
+																			<!--end::Input-->
+																		</div>
+																	</div>
+																	<div class="col" data-kt-calendar="datepicker">
+																		<div class="fv-row mb-9">
+																			<!--begin::Label-->
+																			<label class="fs-6 fw-bold mb-2">Event End Time</label>
+																			<!--end::Label-->
+																			<!--begin::Input-->
+																			<input class="form-control form-control-solid" name="calendar_event_end_time" placeholder="Pick a end time" id="kt_calendar_datepicker_end_time" />
+																			<!--end::Input-->
+																		</div>
+																	</div>
+																</div>
+																<!--end::Input group-->
+															</div>
+															<!--end::Modal body-->
+															<!--begin::Modal footer-->
+															<div class="modal-footer flex-center">
+																<!--begin::Button-->
+																<button type="reset" id="kt_modal_add_event_cancel" class="btn btn-light me-3">Cancel</button>
+																<!--end::Button-->
+																<!--begin::Button-->
+																<button type="button" id="kt_modal_add_event_submit" class="btn btn-primary">
+																	<span class="indicator-label">Submit</span>
+																	<span class="indicator-progress">Please wait...
+																		<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+																</button>
+																<!--end::Button-->
+															</div>
+															<!--end::Modal footer-->
+														</form>
+														<!--end::Form-->
+													</div>
+												</div>
+											</div>
+<!-- ------------------------------------------------------------------------------------- -->
+
+											<!-- Metronic JS -->
+											<script src="path/to/metronic/assets/js/scripts.bundle.js"></script>
+											<!-- FullCalendar JS -->
+											<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
+											<!-- jQuery -->
+											<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+											<script>
+												$(document).ready(function() {
+													const calendarEl = document.getElementById('calendar');
+
+													// Inicializar FullCalendar
+													const calendar = new FullCalendar.Calendar(calendarEl, {
+														initialView: 'dayGridMonth',
+														selectable: true,
+														editable: true,
+
+														// Cargar eventos desde el servidor
+														events: function(fetchInfo, successCallback, failureCallback) {
+
+														},
+
+														// Añadir evento
+														select: function(info) {
+															const title = prompt('Título del evento:');
+															if (title) {
+
+															}
+														},
+
+														// Eliminar evento
+														eventClick: function(info) {
+															if (confirm('¿Deseas eliminar este evento?')) {
+
+															}
+														}
+													});
+
+													calendar.render();
+												});
+											</script>
+
+
+
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 
-						<!-- Metronic JS -->
-						<script src="path/to/metronic/assets/js/scripts.bundle.js"></script>
-						<!-- FullCalendar JS -->
-						<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
-						<!-- jQuery -->
-						<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+
+
+
 
 					</div>
 				</div>
